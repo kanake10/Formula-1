a2361aa1984846c1a85d27afbaac5d1fb7592dd6
kanake10 <10kanake@gmail.com>
2022-08-08 15:57:24 +0300

    add points view

--- app/src/main/java/com/example/formula1/ui/driversstanding/DriversListItem.kt
+++ app/src/main/java/com/example/formula1/ui/driversstanding/DriversListItem.kt
@@ -1,5 +1,6 @@
 package com.example.formula1.ui.driversstanding
 
+
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -7,7 +8,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.example.formula1.domain.models.Driver
@@ -29,6 +34,13 @@ fun DriversListItem(drivers: Driver) {
             text = "${drivers.POS}. ${drivers.Name} (${drivers.Country})",
             style = MaterialTheme.typography.body1,
             overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            text = "${drivers.PTS} PTS",
+            fontStyle = FontStyle.Italic,
+            textAlign = TextAlign.End,
+            style = MaterialTheme.typography.body2,
+            modifier = Modifier.align(CenterVertically)
         )
     }
 }
--- app/src/main/java/com/example/formula1/ui/screen/Screen.kt
+++
@@ -1,5 +0,0 @@
-package com.example.formula1.ui.screen
-
-sealed class Screen(val route: String) {
-    object DriversStandingScreen: Screen("drivers_standing_screen")
-}
