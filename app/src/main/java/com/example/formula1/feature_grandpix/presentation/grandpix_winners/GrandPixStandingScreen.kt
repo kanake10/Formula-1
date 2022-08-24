f284dcdc591586e5cf17471110f37c0d57ad7e9b
kanake10 <10kanake@gmail.com>
2022-08-25 00:47:09 +0300

    viewmodel

---
+++ app/src/main/java/com/example/formula1/feature_grandpix/presentation/grandpix_winners/GrandPixStandingScreen.kt
@@ -0,0 +1,51 @@
+package com.example.formula1.feature_grandpix.presentation.grandpix_winners
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.formula1.feature_grandpix.presentation.viewmodel.GrandPixViewModel
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Composable
+@Destination(start = true)
+fun GrandPixStandingScreen(
+    viewModel: GrandPixViewModel = hiltViewModel()
+) {
+    val state = viewModel.state.value
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(state.grandpix) { grandpix ->
+                GrandPixListItem(
+                    grandpix = grandpix,
+                )
+            }
+        }
+        if(state.error.isNotBlank()) {
+            Text(
+                text = state.error,
+                color = MaterialTheme.colors.error,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp)
+                    .align(Alignment.Center)
+            )
+        }
+        if(state.isLoading) {
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        }
+    }
+}
+
--- app/src/main/java/com/example/formula1/ui/MainActivity.kt
+++ app/src/main/java/com/example/formula1/ui/MainActivity.kt
@@ -23,7 +23,6 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     DestinationsNavHost(navGraph = NavGraphs.root)
-                    
                 }
             }
         }
